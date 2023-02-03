671650cd2181a47847bbcc8e4071591a26be4f8f
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2023-02-03 11:58:20 +0000

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l                                                                                                                                                                          
+#$ -S /bin/bash                                                                                                                                                                         
+#$ -l h_rt=01:00:00                                                                                                                                                                     
+#$ -l mem=4.5G                                                                                                                                                                          
+#$ -N single_it1b                                                                                                                                                                       
+#$ -pe mpi 80                                                                                                                                                                           
+#$ -A KCL_Admin_rse                                                                                                                                                                     
+#$ -cwd                                                                                                                                                                                 
+
+module purge
+module load gcc-libs/7.3.0
+module load compilers/intel/2020/release
+module load mpi/intel/2019/update6/intel
+module load gerun
+module load gromacs/2020.4/intel-2020
+
+gerun mdrun_mpi -v -deffnm min
