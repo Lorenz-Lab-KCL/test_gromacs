7da17a58d0304cad39e449259ad1421311237c7c
Alejandro Santana-Bonilla <62341142+alejandrosantanabonilla@users.noreply.github.com>
2023-02-03 12:05:54 +0000

    Update job.sh

--- job.sh
+++ job.sh
@@ -8,10 +8,10 @@
 #$ -cwd                                                                                                                                                                                 
 
 module purge
+module load beta-modules
+module load gerun
 module load gcc-libs/7.3.0
 module load compilers/intel/2020/release
 module load mpi/intel/2019/update6/intel
-module load gerun
 module load gromacs/2020.4/intel-2020
-
 gerun mdrun_mpi -v -deffnm min
